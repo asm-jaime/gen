3a1e26b8d8be6ed54194749a6f3ca4cd8e7dedd4
asm-jaime <asm.jaime@gmail.com>
2017-06-03 14:49:26 +1000

    add generate numbers

--- gen.go
+++ gen.go
@@ -6,8 +6,9 @@ import (
 )
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var numberRunes = []rune("0123456789")
 
-// Str retrun a string with n length
+// Str retrun a string with n length letters
 func Str(n int) string {
 	rand.Seed(time.Now().UTC().UnixNano())
 	str := make([]rune, n)
@@ -15,4 +16,19 @@ func Str(n int) string {
 		str[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(str)
+}
+
+// Str retrun a string with n length numbers
+func StrNums(n int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	strn := make([]rune, n)
+	for i := range strn {
+		strn[i] = numberRunes[rand.Intn(len(numberRunes))]
+	}
+
+	if strn[0] == '0' {
+		cutRunes := []rune("123456789")
+		strn[0] = cutRunes[rand.Intn(len(cutRunes))]
+	}
+	return string(strn)
 }
--- gen_test.go
+++ gen_test.go
@@ -1,19 +1,37 @@
 package gen
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestStr(t *testing.T) { // {{{
 	str1 := Str(10)
 	str2 := Str(10)
-	fmt.Println(str1)
-	fmt.Println(str2)
+	//fmt.Println(str1)
+	//fmt.Println(str2)
 	if str1 == "" {
 		t.Error("Str empty")
 	}
 	if str1 == str2 {
 		t.Error("Str generated same string")
+	}
+} // }}}
+
+func TestStrNums(t *testing.T) { // {{{
+	str1 := StrNums(10)
+	str2 := StrNums(10)
+	//fmt.Println(str1)
+	//fmt.Println(str2)
+	if str1 == "" {
+		t.Error("StrNums empty")
+	}
+	if str1 == str2 {
+		t.Error("StrNums generated same string")
+	}
+
+	str1_sp := strings.Split(str1, ",")
+	if str1_sp[0] == "0" {
+		t.Error("StrNums string started with 0")
 	}
 } // }}}
